0e18a16e3dc68736736955c5698a76a40eeb1268
Rishu Trivedi <rishu.trivedi1997@gmail.com>
2020-07-28 15:44:42 +0530

    css fixed

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./SortingVisualizer.css";
 
 const minValue = 5;
-const maxValue = 1000;
-const arraySize = 100;
+const maxValue = 700;
+const arraySize = 300;
 
 export default class SortingVisualizer extends React.Component {
   constructor(props) {
@@ -33,13 +33,16 @@ export default class SortingVisualizer extends React.Component {
     const { array } = this.state;
     console.log(array);
     return (
-      <>
+      <div className="array-container">
         {array.map((value, idx) => (
-          <div className="array-bar" key={idx}>
-            {value}
-          </div>
+          <div
+            className="array-bar"
+            key={idx}
+            style={{ height: `${value}px` }}
+          ></div>
         ))}
-      </>
+        <button onClick={() => this.resetArray()}>Generate new array</button>
+      </div>
     );
   }
 }
